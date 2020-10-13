eafe0561760f3ed2c4b0f0779030faaf5adb6e10
chrisreylo73 <72224622+chrisreylo73@users.noreply.github.com>
2020-10-12 19:43:36 -0600

    Fix error reported by Junit for null parameter (Issue #24)

--- src/metrocs/HelloWorld.java
+++ src/metrocs/HelloWorld.java
@@ -22,6 +22,9 @@ public class HelloWorld {
    * @return the constructed greeting.
    */
   public String sayHello(final String toWhom) {
+    if (toWhom == null) {
+         return "Hello!";
+    }
     return "Hello " + toWhom;
   }
 }
