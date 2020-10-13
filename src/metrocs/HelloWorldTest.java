9267c7414ca70fc7dacf66ed8b0432b635302abf
squid <avoc4d0s@gmail.com>
2020-10-12 23:15:49 -0600

    Test Changes

--- src/metrocs/HelloWorldTest.java
+++ src/metrocs/HelloWorldTest.java
@@ -43,6 +43,22 @@ public class HelloWorldTest {
                   () -> assertEquals("Hello world", hW1.sayHello("world")),
                   () -> assertEquals("Hello", hW1.sayHello("")),
                   () -> assertEquals("Hello earth", hW1.sayHello("earth")),
-                  () -> assertEquals("Hello!", hW1.sayHello(null)));
+                  () -> assertEquals("Hello United States of America", hW1.sayHello("United States of America")));
+                  () -> assertEquals("Hello Untied Kingdom", hW1.sayHello("United Kingdom")));
+                  () -> assertEquals("Hello Colorado", hW1.sayHello("Colorado")));
+                  () -> assertEquals("Hello Highway 285", hW1.sayHello("Highway 285")));
+                  () -> assertEquals("Hello Rocky Mountains", hW1.sayHello("Rocky Mountains")));
+                  () -> assertEquals("Hello 1200", hW1.sayHello("1200")));
+                  () -> assertEquals("Hello !@#$%^&*()", hW1.sayHello("!@#$%^&*()")));
+                  () -> assertEquals("Hello 100 West Pine Street, Denver CO", hW1.sayHello("100 West Pine Street, Denver CO")));
+                  () -> assertEquals("Hello (insert name)", hW1.sayHello("(insert name)")));
+                  () -> assertEquals("Hello 18 500", hW1.sayHello("18 500")));
+
+    }
+
+    @Test
+    public void nonAsciiStringTest(){
+	metrocs.HelloWorld h1 = new metrocs.HelloWorld();
+	assertEquals("Hello இந்தியா", hw1.sayHello("இந்தியா"));
     }
 }
