28f2b4226af2ab3b436c3e270454dfe879f63c60
Cmeek <cmeek1@msudenver.edu>
2021-10-13 00:16:15 -0600

    added three tests for HelloAnyoneTest based on understanding of code.

--- src/metrocs/HelloAnyoneTest.java
+++ src/metrocs/HelloAnyoneTest.java
@@ -1,12 +1,32 @@
 package metrocs;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Tests for class HelloAnyone.
  *
- * @author  CS3250 Fall 2021
+ * @author  Connor Meek
  * @version 1.0
  */
 public class HelloAnyoneTest {
-    //TODO: Write tests for HelloAnyone
+    @Test
+    public void sayHelloTest(){
+        metrocs.HelloAnyone Ha1 = new metrocs.HelloAnyone();
+        assertEquals("A special hello to World", Ha1.sayHello("World"));
+        assertEquals("Hello!", Ha1.sayHello(null));
+        assertEquals("Hello world", Ha1.sayHello("world"));
+    }
+
+    @Test
+    public void HelloAnyoneLowerCaseTest(){
+        metrocs.HelloAnyone Ha1 = new metrocs.HelloAnyone();
+        assertEquals("Hello world", Ha1.sayHello("world"));
+    }
+
+    @Test
+    public void HelloAnyoneMinLength(){
+        metrocs.HelloAnyone Ha1 = new metrocs.HelloAnyone();
+        assertEquals("A special hello to ", Ha1.sayHello(""));
+    }
 }
