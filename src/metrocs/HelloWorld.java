6e3ae09c2d50cd5fd6fa02501e4cfe12a4a9afaa
bishnu19 <63917947+bishnu19@users.noreply.github.com>
2021-10-13 11:38:57 -0600

    Update HelloWorld.java

--- src/metrocs/HelloWorld.java
+++ src/metrocs/HelloWorld.java
@@ -1,4 +1,5 @@
 package metrocs;
+
 /**
  * Expansion of classic Hello World program
  * to support demonstration of unit testing.
@@ -8,7 +9,13 @@ package metrocs;
  */
 public class HelloWorld {
   /**
+   * A greeting string variable that can be modified as needed.
+   */
+  public static final String GREETING = "Hello";
+
+  /**
    * Produces desired string on stdout.
+   *
    * @param args ignored
    */
   public static void main(final String[] args) {
@@ -18,25 +25,16 @@ public class HelloWorld {
 
   /**
    * Constructs the greeting.
+   *
    * @param toWhom to whom the greeting is directed
    * @return the constructed greeting.
    */
   public String sayHello(final String toWhom) {
     if (toWhom == null) {
-         return "Hello!";
+      return GREETING + "!";
     } else if (toWhom.equals("")) {
-         return "Hello";
-    } else if (toWhom == "usa") {
-      return " USA is the most powerful country in the world";
-    } else if ( len(toWhom) <= 4) {
-      return " This is a string with length less than four";
-    } else if ( len(toWhom) <= Integer.MAX_VALUE/4 ) {               
-      try { return " This function can print a string with length a quarter of its maximum size";      
-      }
-      catch { return " String exceeds a quarter of its maximum size";     
-      }
+      return GREETING;
     }
-    
-    return "Hello " + toWhom;
+    return GREETING + " " + toWhom;
   }
 }
