e045a99489fc65ec454a4275c12cbe20f8d5ffaf
ecarril6 <91155052+ecarril6@users.noreply.github.com>
2021-10-14 16:16:48 -0600

    Update HelloAnyoneTest.java

--- src/metrocs/HelloAnyoneTest.java
+++ src/metrocs/HelloAnyoneTest.java
@@ -8,5 +8,18 @@ import org.junit.jupiter.api.Test;
  * @version 1.0
  */
 public class HelloAnyoneTest {
-    //TODO: Write tests for HelloAnyone
+    //TODOd: Write tests for HelloAnyone
+     @Test
+     public void sayHelloBasicsTest() {
+         metrocs.HelloAnyone ha1 = new metrocs.HelloAnyone();
+         assertEquals("A special welcome to world.", ha1.sayHello("world"));
+         assertEquals("A special welcome to earth.", ha1.sayHello("earth"));
+
+    }
+
+    @Test
+    public void helloEveryoneEmptyString(){
+        metrocs.HelloAnyone ha1 = new metrocs.HelloAnyone();
+        assertEquals("Hello world", ha1.sayHello(""));
+    }
 }
