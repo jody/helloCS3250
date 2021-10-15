aef201d48cdff3d8d8fcbf8c8abe2ea9cf35daf8
Christian Mudd <lonespartan12@hotmail.com>
2021-10-15 18:05:42 -0400

    HelloAnyone Unit Test
    Added unit test for the HelloAnyoneTest file.

--- src/metrocs/HelloAnyoneTest.java
+++ src/metrocs/HelloAnyoneTest.java
@@ -1,4 +1,8 @@
 package metrocs;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 
 /**
@@ -9,4 +13,37 @@ import org.junit.jupiter.api.Test;
  */
 public class HelloAnyoneTest {
     //TODO: Write tests for HelloAnyone
+     @Test
+    public void sayHelloBasicsTest() {
+        metrocs.HelloAnyone hA1 = new metrocs.HelloAnyone();
+        assertEquals("Hello world", hA1.sayHello("world"));
+        assertEquals("Hello earth", hA1.sayHello("earth"));
+        assertEquals("Hello Bill", hA1.sayHello("Bill"));
+    }
+    @Test
+    public void sayHelloEmptyStringTest() {
+        metrocs.HelloAnyone hA1 = new metrocs.HelloAnyone();
+        assertEquals("Hello", hA1.sayHello(""));
+    }
+
+    @Test
+    public void sayHelloNullTest() {
+        metrocs.HelloAnyone hA1 = new metrocs.HelloAnyone();
+        assertEquals("Hello!", hA1.sayHello(null));
+    }
+
+    @Test
+    public void unnecessaryTest() {
+        assertNotNull(new metrocs.HelloAnyone());
+    }
+
+    @Test
+    public void groupedAssertionTest() {
+        metrocs.HelloAnyone hA1 = new metrocs.HelloAnyone();
+        assertAll("hello someone",
+                  () -> assertEquals("Hello world", hA1.sayHello("world")),
+                  () -> assertEquals("Hello", hA1.sayHello("")),
+                  () -> assertEquals("Hello earth", hA1.sayHello("earth")),
+                  () -> assertEquals("Hello!", hA1.sayHello(null)));
+    }
 }
